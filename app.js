480f5a1f0c3703c32eee9e252a96bd6be7c3b799
jorgelinadev <jorgelinalberti@gmail.com>
2022-07-08 08:59:47 -0300

    First commit

---
+++ app.js
@@ -0,0 +1,11 @@
+//alert ("Mi archivo está bien linkeado")
+
+const nombre = prompt ('Ingresa tu nombre');
+const apellido = prompt ('Ingresa tu apellido');
+const edad = prompt ('Ingresa tu edad');
+const nacionalidad = prompt ('Ingresa tu nacionalidad');
+const documento = prompt ('Ingresa tu número de documento');
+
+alert ('Nuevo usuario ingresado al sistema: Nombre:' + nombre +'; Apellido: '+ apellido+'; Edad: ' + edad +'; Nacionalidad: '+ nacionalidad +'; Número de documento: '+ documento );
+
+console.log (alert);
